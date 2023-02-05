5672e0410e7abc3e3bccf098af2813b8bd64bf96
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-05 21:28:17 +0100

    Fix test environment

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
-  testMatch: ["**/src/**/*.test.ts"],
+  testEnvironment: "node",
+  testMatch: ["**/src/**/*.test.ts", "!src/**/index.ts"],
   resolver: "jest-ts-webcompat-resolver",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/index.ts",
+    "!src/loadEnvironment.ts",
+    "!src/**/index.ts",
+    "!src/server/startServer.ts",
+    "!src/database/connectDb.ts",
+  ],
 };
